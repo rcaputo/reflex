676bb42fec028e9fb481022f3742ff6813281fc5
Rocco Caputo <rcaputo@cpan.org>
2009-08-29 19:24:31 -0400

    Fix tab damage.

--- eg-10-setup.pl
+++ eg-10-setup.pl
@@ -1,51 +1,51 @@
 #!/usr/bin/perl
 
 {
-  package Counter;
-  use Moose;
-  extends 'Stage';
-  use Delay;
-  use ObserverTrait;
-  use EmitterTrait;
+	package Counter;
+	use Moose;
+	extends 'Stage';
+	use Delay;
+	use ObserverTrait;
+	use EmitterTrait;
 
-  has count   => (
-    traits    => ['Emitter'],
-    isa       => 'Int',
-    is        => 'rw',
-    default   => 0,
-  );
+	has count   => (
+		traits    => ['Emitter'],
+		isa       => 'Int',
+		is        => 'rw',
+		default   => 0,
+	);
 
-  has ticker  => (
-    traits    => ['Observer'],
-    isa       => 'Delay',
-    is        => 'rw',
-    setup     => sub { Delay->new( interval => 1, auto_repeat => 1 ) },
-  );
+	has ticker  => (
+		traits    => ['Observer'],
+		isa       => 'Delay',
+		is        => 'rw',
+		setup     => sub { Delay->new( interval => 1, auto_repeat => 1 ) },
+	);
 
-  sub on_ticker_tick {
-    my $self = shift;
-    $self->count($self->count() + 1);
-  }
+	sub on_ticker_tick {
+		my $self = shift;
+		$self->count($self->count() + 1);
+	}
 }
 
 {
-  package Watcher;
-  use Moose;
-  extends 'Stage';
+	package Watcher;
+	use Moose;
+	extends 'Stage';
 
-  has counter => (
-    traits  => ['Observer'],
-    isa     => 'Counter|Undef',
-    is      => 'rw',
-    setup   => sub { Counter->new() },
-  );
+	has counter => (
+		traits  => ['Observer'],
+		isa     => 'Counter|Undef',
+		is      => 'rw',
+		setup   => sub { Counter->new() },
+	);
 
-  sub on_counter_count {
-    my ($self, $args) = @_;
-    warn "Watcher sees counter count: $args->{value}\n";
+	sub on_counter_count {
+		my ($self, $args) = @_;
+		warn "Watcher sees counter count: $args->{value}\n";
 
 		$self->counter(undef) if $args->{value} >= 5;
-  }
+	}
 }
 
 my $w = Watcher->new();
