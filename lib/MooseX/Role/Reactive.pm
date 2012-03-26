8c5b88149fd2beba7b18521c23283d0d4653cf9f
Rocco Caputo <rcaputo@cpan.org>
2012-03-26 01:32:07 -0400

    Reserve MooseX::Role::Reactive for the event-loop agnostic part of Reflex.

---
+++ lib/MooseX/Role/Reactive.pm
@@ -0,0 +1,9 @@
+pacakage MooseX::Role::Reactive;
+
+use Moose::Role;
+
+# This is a namespace reservation for the emit/watch part of
+# Reflex::Role::Reactive.  The MooseX::Role should be event-loop
+# agnostic somehow.  See docs/TODO.otl in Reflex's repository.
+
+1;
